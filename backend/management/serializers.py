9027ebdd62bedb5fe7c81c358d7a010da10446ae
mbohngawur <mbohlah.ngawur@gmail.com>
2025-07-23 18:06:07 +0700

    Add backend/management/serializers.py 1753268766152

---
+++ backend/management/serializers.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+from .models import User, Project
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'role', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        user = User(**validated_data)
+        if password:
+            user.set_password(password)
+        user.save()
+        return user
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None)
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        if password:
+            instance.set_password(password)
+        instance.save()
+        return instance
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = ['id', 'name', 'description', 'start_date', 'end_date', 'created_by']
+        read_only_fields = ['created_by']
